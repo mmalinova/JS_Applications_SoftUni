5180e1bbe1c35bf9433b749352a61360ab98611c
mmalinova <malinova29@gmail.com>
2021-03-04 20:48:09 +0200

    Git commit some exercises.

--- JS-Applications-Asynchronous-Programming/02.Bus-Schedule/app.js
+++ JS-Applications-Asynchronous-Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,35 @@
 function solve() {
+    const info = document.getElementById('info').children[0];
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    
+    let id = 'depot';
+    let data;
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        try {
+            const url = `http://localhost:3030/jsonstore/bus/schedule/${id}`;
+            const response = await fetch(url);
+            if (response.ok == false) {
+                throw new Error('Error');
+            }
+            data = await response.json();
+            info.textContent = `Next stop ${data.name}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+
+            id = data.next;
+        } catch (error) {
+            info.textContent = 'Error';
+            arriveBtn.disabled = true;
+            departBtn.disabled = true;
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${data.name}`;
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
     }
 
     return {
